b0c39c83b531508ef7a3ab112a09dd8314d60f15
Kedar Haldar <113050951+HaldarKedar@users.noreply.github.com>
2023-04-18 22:13:20 -0700

    Started pathogen

--- Capstone-2023/src/Pathogen.java
+++ Capstone-2023/src/Pathogen.java
@@ -1,20 +1,35 @@
+import java.awt.*;
+import java.lang.*;
 
 /*
  * Comment header
  */
 
 
-import java.lang.Math;
 
 public abstract class Pathogen {
-
-	private int posX;
-	private int posY = Math.random(10);
 	
-	public Pathogen(Image pathogen) {
+	private int x;
+	private int y;
+	private Image pathogen;
+	
+	public Pathogen(int x, int y, Image pathogen) {
 		
+		this.x = x;
+		this.y = y;
+		this.pathogen = pathogen;
 		
 		
 	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	
+	public abstract void attack();
+	
+	public abstract void targetPlayer();
+	
 	
 }
